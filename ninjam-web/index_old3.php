5959ceb80ee2f9096f1c4551c0897385be534fc8
Ayvan <ajvan.ivan@gmail.com>
2018-11-22 17:24:28 +0300

    new web-page style, update player style

--- ninjam-web/index_old3.php
+++ ninjam-web/index_old3.php
@@ -58,7 +58,7 @@
 </header>
 <div class="container">
     <div class="py-5">
-        <div id="jp_container_N" style="width: 300px; display: table; margin: 0 auto;" class="jp-audio"
+        <div id="jp_container_N" style="display: table; margin: 0 auto;" class="jp-audio"
              role="application" aria-label="media player">
             <div class="jp-type-playlist">
                 <div id="jquery_jplayer_N" class="jp-jplayer"></div>
@@ -66,24 +66,24 @@
                     <div class="jp-video-play">
                         <button class="jp-video-play-icon" role="button" tabindex="0">play</button>
                     </div>
-                    <div class="jp-interface" style="width: 300px;">
-                        <div class="jp-progress" style="left: 120px; top: 20px; width: 150px;">
+                    <div class="jp-interface">
+                        <div class="jp-progress">
                             <div class="jp-seek-bar">
                                 <div class="jp-play-bar"></div>
                             </div>
                         </div>
-                        <div class="jp-current-time" style="font-size: 1em; width: 60px" role="timer" aria-label="time">&nbsp;</div>
-                        <div class="jp-controls-holder" style="width: 300px;">
-                            <div class="jp-controls" style="padding-top: 10px; width: 150px;">
+                        <div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
+                        <div class="jp-controls-holder">
+                            <div class="jp-controls">
                                 <!--button class="jp-previous" role="button" tabindex="0">previous</button-->
                                 <button class="jp-play" role="button" tabindex="0">play</button>
                                 <!--button class="jp-next" role="button" tabindex="0">next</button-->
                                 <button class="jp-stop" role="button" tabindex="0">stop</button>
                             </div>
-                            <div class="jp-volume-controls" style="left: 123px; width: 200px;">
+                            <div class="jp-volume-controls">
                                 <button class="jp-mute" role="button" tabindex="0">mute</button>
-                                <button class="jp-volume-max" style="left: 128px;" role="button" tabindex="0">max volume</button>
-                                <div class="jp-volume-bar"  style="width: 100px;">
+                                <button class="jp-volume-max" role="button" tabindex="0">max volume</button>
+                                <div class="jp-volume-bar">
                                     <div class="jp-volume-bar-value"></div>
                                 </div>
                             </div>
