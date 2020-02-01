be20c2371256257cb501ed6cf1804bcc136bba3f
Ayvan <ajvan.ivan@gmail.com>
2020-02-01 20:55:57 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -f Dockerfile.builder -t builder .
-docker build -f Dockerfile.server -t server .
+docker build -f Dockerfile.server -t ubuntu-server .
 docker build --no-cache -f Dockerfile -t ninjam-server .
